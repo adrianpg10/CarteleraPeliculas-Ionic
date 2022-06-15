f8c97d05b33fc5c5a029d96d4d92f174c2d14cce
adrianpg10 <adrianperezgomezsmr@gmail.com>
2022-06-15 18:40:46 +0200

    Cartelera Peliculas

---
+++ src/app/services/data-local.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { PeliculaDetalle } from '../interfaces/interfaces';
+import { Storage } from '@ionic/storage-angular';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataLocalService {
+  private _storage: Storage | null = null;
+  peliculas: PeliculaDetalle[] = [];
+
+  constructor(private storage: Storage, private toastCtrl: ToastController) {
+    this.init();
+    this.cargarFavoritos();
+  }
+
+  //Controlar peliculas duplicadas
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1500
+    });
+    toast.present();
+  }
+
+  guardarPelicula(pelicula: PeliculaDetalle) {
+
+    let existe = false;
+    let mensaje = "";
+    for (const peli of this.peliculas) {
+
+      if (peli.id === pelicula.id) {
+        existe = true;
+        break;
+
+      }
+    }
+    if (existe) {
+      this.peliculas = this.peliculas.filter(peli => peli.id !== pelicula.id);
+      mensaje = "Removido de Favoritos";
+    } else {
+      this.peliculas.push(pelicula);
+      mensaje = "Agregada a favoritos";
+    }
+
+    this.presentToast(mensaje);
+    this.storage.set("peliculas", this.peliculas);
+    return !existe;
+  }
+
+  async cargarFavoritos() {
+    const peliculas = await this.storage.get("peliculas");
+    this.peliculas = peliculas || [];
+    return this.peliculas;
+  }
+  async existePelicula(id) {
+    id = Number(id);
+    await this.cargarFavoritos();
+    const existe = this.peliculas.find(peli=> peli.id===id);
+    return (existe) ? true:false;
+  }
+
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  url:"https://api.themoviedb.org/3",
+  apiKey: "3dd19f6c17decfac4e352b80d5bf622a",
+  imgPath: "https://image.tmdb.org/t/p"
+};
